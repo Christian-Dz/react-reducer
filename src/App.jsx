9165e7e5452f48a8a2c60c0cd034b6c66b0bb2fa
Christian-Dz <131562603+Christian-Dz@users.noreply.github.com>
2023-11-20 18:26:45 -0600

    react reducer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useReducer } from 'react'
+import { Contador } from './Components/Contador'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Contador/>
+    </>
+  )
+}
+
+export default App
